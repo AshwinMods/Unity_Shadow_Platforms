32753e4fd2f01e8db47f6ba45b8a33e2b363f6f3
AshwinMods <ashwinmods@gmail.com>
2020-12-16 22:25:48 +0530

    Clipping Triangle outside Corner

--- Assets/Scripts/ProjectMesh.cs
+++ Assets/Scripts/ProjectMesh.cs
@@ -9,6 +9,7 @@ public class ProjectMesh : MonoBehaviour
 	[SerializeField] Transform plainCorner;
 
 	[Space]
+	[SerializeField] bool useClip = false;
 	[SerializeField] bool project = false;
 	[System.Serializable]
 	struct ProjectData
@@ -39,7 +40,10 @@ public class ProjectMesh : MonoBehaviour
 		{
 			for (int j = 0; j < projectData[i].plains.Length; j++)
 			{
-				var projection = Calc_Projection(targetMesh, lightDirection.position, projectData[i].plains[j].position, projectData[i].plains[j].forward);
+				var projection = Calc_Projection(targetMesh, lightDirection.position, 
+					projectData[i].plains[j].position, projectData[i].plains[j].forward, 
+					plainCorner.position);
+
 				for (int k = 0; k < projection.Count; k++)
 				{
 					Transform t;
@@ -62,7 +66,7 @@ public class ProjectMesh : MonoBehaviour
 					t.SetPositionAndRotation(Vector3.zero, Quaternion.identity);
 					c.SetPath(0, projection[k]);
 				}
-				for (int l = projection.Count; l < transform.childCount; l++)
+				for (int l = transform.childCount-1; l >= projection.Count; --l)
 				{
 					if (Application.isPlaying)
 						Destroy(transform.GetChild(l).gameObject);
@@ -73,7 +77,7 @@ public class ProjectMesh : MonoBehaviour
 		}
 	}
 
-	List<List<Vector2>> Calc_Projection(Mesh mesh, Vector3 direction, Vector3 plain, Vector3 plainNormal)
+	List<List<Vector2>> Calc_Projection(Mesh mesh, Vector3 direction, Vector3 plain, Vector3 plainNormal, Vector3 clip)
 	{
 		List<List<Vector2>> ret = new List<List<Vector2>>(mesh.triangles.Length / 3);
 		for (int j = 0; j < mesh.triangles.Length; j+=3)
@@ -88,10 +92,82 @@ public class ProjectMesh : MonoBehaviour
 			var c = Calc_Projection(v, (v - direction), plain, plainNormal);
 
 			List<Vector2> points = new List<Vector2>(3);
-			points.Add(a);
-			points.Add(b);
-			points.Add(c);
-			ret.Add(points);
+			if (useClip)
+			{
+				bool aOut = IsOutSide(a);
+				bool bOut = IsOutSide(b);
+				bool cOut = IsOutSide(c);
+				if (aOut && bOut && cOut)
+				{
+					continue;
+				}
+				else
+				if (aOut && bOut ||
+					bOut && cOut ||
+					aOut && cOut)
+				{
+					var out1 = aOut ? a : bOut ? b : c;
+					var out2 = cOut ? c : bOut ? b : a;
+					var in1 = !aOut ? a : !bOut ? b : c;
+
+					var dClip = (clip - in1);
+
+					var dVert = (out1 - in1);
+					var tVert = dClip.x / dVert.x;
+					out1 = in1 + dVert * tVert;
+
+					dVert = (out2 - in1);
+					tVert = dClip.x / dVert.x;
+					out2 = in1 + dVert * tVert;
+
+					points.Add(out1);
+					points.Add(out2);
+					points.Add(in1);
+					ret.Add(points);
+				}
+				else
+				if (aOut || bOut || cOut)
+				{
+					var out1 = aOut ? a : bOut ? b : c;
+					var in1 = !aOut ? a : !bOut ? b : c;
+					var in2 = !cOut ? c : !bOut ? b : a;
+
+
+					var dClip = (clip - in1);
+					var dVert = (out1 - in1);
+					var tVert = dClip.x / dVert.x;
+					var nVert1 = in1 + dVert * tVert;
+
+					points.Add(nVert1);
+					points.Add(in1);
+					points.Add(in2);
+					ret.Add(points);
+
+					dClip = (clip - in2);
+					dVert = (out1 - in2);
+					tVert = dClip.x / dVert.x;
+					var nVert2 = in2 + dVert * tVert;
+
+					points.Clear();
+					points.Add(nVert1);
+					points.Add(nVert2);
+					points.Add(in2);
+					ret.Add(points);
+				}
+				else
+				{
+					points.Add(a);
+					points.Add(b);
+					points.Add(c);
+					ret.Add(points);
+				}
+			}else
+			{
+				points.Add(a);
+				points.Add(b);
+				points.Add(c);
+				ret.Add(points);
+			}
 		}
 		return ret;
 	}
@@ -99,5 +175,15 @@ public class ProjectMesh : MonoBehaviour
 	Vector3 Calc_Projection(Vector3 v, Vector3 d, Vector3 p, Vector3 n)
 	{
 		return (v + Vector3.Dot((p - v), n) / Vector3.Dot(d, n) * d);
+	}
+
+	bool IsOutSide(Vector2 v)
+	{
+		return (v.x < plainCorner.position.x);
+	}
+
+	void Triangle_Clip(ref Vector3 a, ref Vector3 b, ref Vector3 c, ref Vector3 clip)
+	{
+
 	}
 }
